3c03f849bc99c9c34d8a3648a86c7b13c9c62685
seojeongHHHui <sjhui0523@gmail.com>
2024-06-14 10:47:37 +0900

    container / Annotation Scanning Configuration

---
+++ container/src/test/java/com/poscodx/container/soundsystem/CDPlayerXmlConfigTest.java
@@ -0,0 +1,27 @@
+package com.poscodx.container.soundsystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations= {"classpath:com/poscodx/container/config/soundsystem/applicationContext.xml"})
+public class CDPlayerXmlConfigTest {
+	@Autowired
+	CDPlayer cdPlayer;
+	
+	@Test
+	public void testCDPlayNotNull() {
+		assertNotNull(cdPlayer);
+	}
+	
+	@Test
+	public void testPlay() {
+		assertEquals("Playing 붕붕 by 김하온", cdPlayer.play());
+	}
+}
