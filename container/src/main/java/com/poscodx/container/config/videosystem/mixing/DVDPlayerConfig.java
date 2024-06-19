f0fb977ef6a4a54ac3cd93e0e2ae9982f1e88554
seojeongHHHui <sjhui0523@gmail.com>
2024-06-19 09:34:27 +0900

    Container - Bean Configuration #finish

---
+++ container/src/main/java/com/poscodx/container/config/videosystem/mixing/DVDPlayerConfig.java
@@ -0,0 +1,22 @@
+package com.poscodx.container.config.videosystem.mixing;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.poscodx.container.videosystem.DVDPlayer;
+import com.poscodx.container.videosystem.DigitalVideoDisc;
+
+/**
+ *  JavaConfig2  <----  JavaConfig1
+ *               import
+ */
+@Configuration
+@Import({DVDConfig.class})
+public class DVDPlayerConfig {
+	@Bean
+	public DVDPlayer dvdPlayer(@Qualifier("avengers") DigitalVideoDisc dvd) {
+		return new DVDPlayer(dvd);
+	}
+}
--- container/src/test/java/com/poscodx/container/videosystem/DVDPlayerXmlConfigTest.java
+++ container/src/test/java/com/poscodx/container/videosystem/DVDPlayerXmlConfigTest.java
@@ -1,5 +1,6 @@
 package com.poscodx.container.videosystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Disabled;
@@ -35,6 +36,51 @@ public class DVDPlayerXmlConfigTest {
 	@Qualifier("avengersAgeOfUltron")
 	DigitalVideoDisc dvd5;
 
+	@Autowired
+	@Qualifier("avengersCaptainAmerica")
+	DigitalVideoDisc dvd6;
+
+	@Autowired
+	@Qualifier("avengersDirectorEdition")
+	DigitalVideoDisc dvd7;
+
+	@Autowired
+	DVDPack dvdPack;
+
+	@Autowired
+	@Qualifier("avengersExpansionEdition1")
+	DigitalVideoDisc dvd8;
+
+	@Autowired
+	@Qualifier("avengersExpansionEdition2")
+	DigitalVideoDisc dvd9;
+
+	@Autowired
+	@Qualifier("avengersExpansionEdition3")
+	DigitalVideoDisc dvd10;
+
+	@Autowired
+	@Qualifier("dvdPlayer1")
+	DVDPlayer dvdPlayer1;
+
+	@Autowired
+	@Qualifier("dvdPlayer2")
+	DVDPlayer dvdPlayer2;
+
+	@Autowired
+	@Qualifier("dvdPlayer3")
+	DVDPlayer dvdPlayer3;
+
+	@Autowired
+	@Qualifier("dvdPlayer4")
+	DVDPlayer dvdPlayer4;
+
+	@Autowired
+	@Qualifier("dvdPlayer5")
+	DVDPlayer dvdPlayer5;
+
+	//////////////////////////////////////////////
+	
 	@Disabled
 	@Test
 	public void testDVD1() {
@@ -59,6 +105,61 @@ public class DVDPlayerXmlConfigTest {
 	@Test
 	public void testDVD5() {
 		assertNotNull(dvd5);
+	}
+
+	@Test
+	public void testDVD6() {
+		assertNotNull(dvd6);
+	}
+
+	@Test
+	public void testDVD7() {
+		assertNotNull(dvd7);
+	}
+	
+	@Test
+	public void testDVDPack() {
+		assertNotNull(dvdPack);
+	}	
+
+	@Test
+	public void testDVD8() {
+		assertNotNull(dvd8);
+	}
+
+	@Test
+	public void testDVD9() {
+		assertNotNull(dvd9);
+	}
+
+	@Test
+	public void testDVD10() {
+		assertNotNull(dvd10);
+	}
+
+	@Test
+	public void testPlay1() {
+		assertEquals("Playing Movie Marvel's Iron Man", dvdPlayer1.play());
+	}
+	
+	@Test
+	public void testPlay2() {
+		assertEquals("Playing Movie Marvel's Iron Man", dvdPlayer2.play());
+	}
+
+	@Test
+	public void testPlay3() {
+		assertEquals("Playing Movie Marvel's Iron Man", dvdPlayer3.play());
+	}
+
+	@Test
+	public void testPlay4() {
+		assertEquals("Playing Movie Marvel's Iron Man", dvdPlayer4.play());
+	}
+
+	@Test
+	public void testPlay5() {
+		assertEquals("Playing Movie Marvel's Iron Man", dvdPlayer5.play());
 	}
 	
 }
